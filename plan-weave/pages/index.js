bbc163317f6840cde77f3dced98d16a3aeb5a01a
Connor Keenum <Nellakprime@gmail.com>
2023-05-03 13:32:17 -0500

    Changed index

--- plan-weave/pages/index.js
+++ plan-weave/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          I Hate <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
