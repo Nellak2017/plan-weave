99a2bf622142eae3fc8610688f210c8b29a7fdcf
Connor Keenum <Nellakprime@gmail.com>
2023-05-10 18:32:59 -0500

    Testing useAuthState hook from react-firebase-hooks/auth

--- plan-weave/pages/plan-weave.js
+++ plan-weave/pages/plan-weave.js
@@ -1,17 +1,21 @@
 import { React, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { signOutOfApp } from '../firebase/firebase_auth'
-import { onAuthStateChanged } from 'firebase/auth'
+//import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../firebase/firebase_auth.js'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 function PlanWeave() {
 	const router = useRouter()
-	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const [user, loading] = useAuthState(auth)
+	//const [loggedIn, setLoggedIn] = useState(isLoggedIn)
 
+	/*
 	// Listen for authentication state changes
 	useEffect(() => {
-		return () => onAuthStateChanged(auth, user => user ? setIsLoggedIn(true) : setIsLoggedIn(false))
-	}, [isLoggedIn])
+		return () => onAuthStateChanged(auth, user => user ? setLoggedIn(true) : setLoggedIn(false))
+	}, [loggedIn])
+	*/
 
 	const handleLogout = async () => {
 		try {
@@ -23,7 +27,7 @@ function PlanWeave() {
 	}
 
 	return (
-		isLoggedIn ? (
+		user ? (
 			<>
 				<nav>
 					<button onClick={handleLogout}>Log out</button>
@@ -36,6 +40,20 @@ function PlanWeave() {
 				<p>You need to log in to access this page.</p>
 			</div>
 	)
+}
+
+export async function getServerSideProps(context) {
+	const { req } = context
+	const sessionCookie = req.cookies.session || ''
+
+
+	try {
+		const user = await auth.verifySessionCookie(sessionCookie)
+		console.log('user ', user)
+		return { props: {isLoggedIn: (user !== null && typeof user !== 'undefined')}}
+	} catch (e) {
+		return {props: {isLoggedIn: false}}
+	}
 }
 
 export default PlanWeave
