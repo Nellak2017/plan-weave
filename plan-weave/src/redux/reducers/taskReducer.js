2e8cdce20be3b52c278ff57689959ffcb4940df0
Connor Keenum <Nellakprime@gmail.com>
2023-09-23 17:27:44 -0500

    Trivial.

--- plan-weave/src/redux/reducers/taskReducer.js
+++ plan-weave/src/redux/reducers/taskReducer.js
@@ -6,17 +6,17 @@ const timestamp = Timestamp.fromDate(new Date()).seconds // used for testing pur
 
 const initialState = {
 	tasks: [
-		{ status: 'incomplete', task: 'Cyber - Study, Quiz Prep, W5, Other', ttc: 3, id: 1, timestamp: timestamp }, 
-		{ status: 'incomplete', task: 'Gym', ttc: 1.5, id: 2, timestamp: timestamp - 1 }, 
-		{ status: 'incomplete', task: 'Shower / Meal prep / Eat', ttc: 1, id: 3, timestamp: timestamp - 2 },
-		{ status: 'incomplete', task: 'ML - A4, Study', ttc: 1.5, id: 4, timestamp: timestamp - 3 },
-		{ status: 'incomplete', task: 'break', ttc: .5, id: 5, timestamp: timestamp - 4 },
-		{ status: 'incomplete', task: 'Span - Mindtap, Disc, Study', ttc: 2, id: 6, timestamp: timestamp - 5 },
+		{ status: 'incomplete', task: 'break', ttc: .75, id: 1, timestamp: timestamp }, 
+		{ status: 'incomplete', task: 'Span - study, mindtap, disc', ttc: 1.5, id: 2, timestamp: timestamp - 1 }, 
+		{ status: 'incomplete', task: 'Lunch', ttc: .5, id: 3, timestamp: timestamp - 2 },
+		{ status: 'incomplete', task: 'Shower +', ttc: .5, id: 4, timestamp: timestamp - 3 },
+		{ status: 'incomplete', task: 'Clean', ttc: .5, id: 5, timestamp: timestamp - 4 },
+		{ status: 'incomplete', task: 'Cyber - text 2-5, fl, a2, slide, lect.', ttc: 2, id: 6, timestamp: timestamp - 5 },
 		{ status: 'incomplete', task: 'break', ttc: .5, id: 7, timestamp: timestamp - 6 },
-		{ status: 'incomplete', task: 'SE II - Discord Plan, Study', ttc: .75, id: 9, timestamp: timestamp - 9 },
-		{ status: 'incomplete', task: 'break', ttc: 1, id: 10, timestamp: timestamp - 10 },
+		{ status: 'incomplete', task: 'SE II - Study, Lectures, Project', ttc: 1, id: 9, timestamp: timestamp - 9 },
+		{ status: 'incomplete', task: 'break', ttc: .75, id: 10, timestamp: timestamp - 10 },
+		{ status: 'incomplete', task: 'ML - A4, Study', ttc: 1.5, id: 11, timestamp: timestamp - 11 },
 		/*
-		{ status: 'incomplete', task: '', ttc: 1, id: 11, timestamp: timestamp - 11 },
 		{ status: 'incomplete', task: '', ttc: 1.5, id: 12, timestamp: timestamp - 12 },
 		{ status: 'incomplete', task: '', ttc: .5, id: 13, timestamp: timestamp - 13 },
 		{ status: 'incomplete', task: '', ttc: .5, id: 14, timestamp: timestamp - 14 },
