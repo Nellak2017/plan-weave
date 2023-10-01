6cdd57b245340b42025fb1a6bd1a2f3eaf6d10b8
Connor Keenum <Nellakprime@gmail.com>
2023-10-01 15:52:48 -0500

    Trivial.

--- plan-weave/src/redux/reducers/taskReducer.js
+++ plan-weave/src/redux/reducers/taskReducer.js
@@ -6,12 +6,12 @@ const timestamp = Timestamp.fromDate(new Date()).seconds // used for testing pur
 
 const initialState = {
 	tasks: [
-		{ status: 'incomplete', task: 'Cyber: Quiz', ttc: .75, id: 1, timestamp: timestamp }, 
-		{ status: 'incomplete', task: 'Cyber: A2', ttc: 2, id: 2, timestamp: timestamp - 1 }, 
-		{ status: 'incomplete', task: 'Span : Study', ttc: 1, id: 3, timestamp: timestamp - 2 },
-		{ status: 'incomplete', task: 'Span : Mindtap', ttc: 2, id: 4, timestamp: timestamp - 3 },
-		{ status: 'incomplete', task: 'Shower +', ttc: .5, id: 5, timestamp: timestamp - 4 },
-		{ status: 'incomplete', task: 'Break', ttc: 2, id: 6, timestamp: timestamp - 5 },
+		{ status: 'incomplete', task: 'Cyber: A2', ttc: .75, id: 1, timestamp: timestamp }, 
+		{ status: 'incomplete', task: 'Span : Study', ttc: 2, id: 2, timestamp: timestamp - 1 }, 
+		{ status: 'incomplete', task: 'Span : Mindtap', ttc: 1, id: 3, timestamp: timestamp - 2 },
+		{ status: 'incomplete', task: 'Meal Prep', ttc: 2, id: 4, timestamp: timestamp - 3 },
+		{ status: 'incomplete', task: 'Break', ttc: .5, id: 5, timestamp: timestamp - 4 },
+		{ status: 'incomplete', task: 'Cyber : Study', ttc: 2, id: 6, timestamp: timestamp - 5 },
 		/*
 		{ status: 'incomplete', task: '', ttc: 1.5, id: 7, timestamp: timestamp - 6 },
 		{ status: 'incomplete', task: 'Cyber - text 2-5, fl, a2, slide, lect.', ttc: 2, id: 9, timestamp: timestamp - 9 },
