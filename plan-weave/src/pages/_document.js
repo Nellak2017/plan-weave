f314214b50b6072fcb548bc685b7fa8d9c71049c
Connor Keenum <Nellakprime@gmail.com>
2024-02-10 20:41:00 -0600

    Fixed FOUC (Flash of Unstyled Content) issue by adding getInitialProps method in Document

--- plan-weave/src/pages/_document.js
+++ plan-weave/src/pages/_document.js
@@ -1,6 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from "styled-components"
 
 class MyDocument extends Document {
+  // Used to eliminate a FOUC, where FOUC is a Flash of Unstyled Content, caused by styled components
+  // https://nimblecode.dev/blog/fixing-fouc-nextjs-styled-components/
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
   render() {
     return (
       <Html lang="en">
