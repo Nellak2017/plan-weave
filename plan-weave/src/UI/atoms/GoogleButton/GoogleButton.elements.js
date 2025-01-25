797821f1dfd2ce03b04d2db418274fd231335a2a
Connor Keenum <nellakprime@gmail.com>
2025-01-24 22:56:14 -0600

    Simplify GoogleButton with MUI styles.

--- plan-weave/src/UI/atoms/GoogleButton/GoogleButton.elements.js
+++ plan-weave/src/UI/atoms/GoogleButton/GoogleButton.elements.js
@@ -1,20 +1,9 @@
-import styled from 'styled-components'
-export const StyledGoogleButton = styled.button`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	column-gap: 24px;
-	background-color: #fff;
-	font-family: 'Roboto', sans-serif;
-	font-weight: bold;
-	font-size: 14px;
-	color: #00000087;
-	box-shadow: ${props => props.theme.elevations.extraSmall};
-	padding: 11px 8px;
-	&:hover {
-		background-color: #fff;
-		color: #4285F4;
-		box-shadow: ${props => props.theme.elevations.small};
-	}
-	&:focus { outline: 2px solid #4285F4; }
-`
+import { styled } from "@mui/material"
+
+export const StyledGoogleButton = styled('button')(({ theme }) => ({
+	backgroundColor: '#fff', color: '#00000087', padding: '11px 8px', boxShadow: `${theme.shadows[1]}`,
+	display: 'flex', alignItems: 'center', justifyContent: 'center', columnGap: '24px',
+	fontFamily: "'Roboto', sans-serif", fontWeight: 'bold', fontSize: '14px',
+	'&:hover': { backgroundColor: '#fff', color: '#4285F4', boxShadow: `${theme.shadows[1]}`,},
+	'&:focus': { outline: '2px solid #4285F4' }
+}))
