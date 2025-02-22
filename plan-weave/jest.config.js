9c7e822ca5ee78417b2ee2a8d3cb82368f163e46
Connor Keenum <nellakprime@gmail.com>
2025-02-21 19:47:55 -0600

    Convert jest.config.js to simplfied form, removing styled-component dependency.

--- plan-weave/jest.config.js
+++ plan-weave/jest.config.js
@@ -1,7 +1,5 @@
 const testConfig = {
-	clearMocks: true,
-	//collectCoverage: true,
-	testEnvironment: "jsdom",
+	clearMocks: true, testEnvironment: "jsdom",
 	transform: {
 		"^.+\\.(t|j)sx?$": [
 			"@swc/jest",
@@ -17,16 +15,6 @@ const testConfig = {
 			}
 		]
 	},
-	transformIgnorePatterns: [
-		"node_modules/(?!(rescript)/)"
-	],
-	moduleNameMapper: {
-		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-			'<rootDir>/__mocks__/fileMock.js',
-		'\\.(css|less)$': 'identity-obj-proxy',
-		'styled-components': 'styled-components/dist/styled-components.browser.cjs.js',
-	}, // https://jestjs.io/docs/webpack#mocking-css-modules
-	// https://github.com/styled-components/styled-components/issues/4081
+	transformIgnorePatterns: ["node_modules/(?!(rescript)/)"],
 }
-
 module.exports = testConfig
