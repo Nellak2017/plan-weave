ea3c773290f957b63503d55c7f009e4379169d7b
Connor Keenum <nellakprime@gmail.com>
2025-02-21 19:46:21 -0600

    Convert storybook preview.js to simplfied form, removing styled-component dependency.

--- plan-weave/.storybook/preview.js
+++ plan-weave/.storybook/preview.js
@@ -1,41 +1,24 @@
-import theme from '../src/UI/styles/theme.js'
-import GlobalStyle from '../src/UI/styles/globalStyles.js'
-import { ThemeProvider, StyleSheetManager } from 'styled-components'
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
+import { theme as MUITheme } from '../src/UI/styles/MUITheme.js'
+import { GlobalStyles } from '@mui/material'
+import { muiGlobalStyles } from '../src/UI/styles/globalStyles.js'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import isPropValid from '@emotion/is-prop-valid'
-
-import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline' // normalizes css for MUI
-import { theme as MUITheme } from '../src/UI/styles/MUITheme.js'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 export default {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-    viewport: {
-      viewports: INITIAL_VIEWPORTS,
-    },
+    controls: { matchers: { color: /(background|color)$/i, date: /Date$/, }, },
+    viewport: { viewports: INITIAL_VIEWPORTS, },
   },
-  // Wrap every story in the story book with a ThemeProvider and the GlobalStyles
   decorators: [
-    (Story) => (
-      <StyleSheetManager shouldForwardProp={prop => isPropValid(prop)}>
-        <MUIThemeProvider theme={MUITheme}>
-          <CssBaseline />
-          <ThemeProvider theme={theme}>
-            <ToastContainer position="bottom-left" autoClose={3000} />
-            <GlobalStyle />
-            <Story />
-          </ThemeProvider>
-        </MUIThemeProvider>
-      </StyleSheetManager>
+    Story => (
+      <MUIThemeProvider theme={MUITheme}>
+        <GlobalStyles styles={muiGlobalStyles({ theme: MUITheme })}/>
+        <ToastContainer position="bottom-left" autoClose={3000} />
+        <Story />
+      </MUIThemeProvider>
     )
   ]
 }
