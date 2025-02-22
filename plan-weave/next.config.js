511413fc62eb331558d708d6a82f68d9a623cdd7
Connor Keenum <nellakprime@gmail.com>
2025-02-21 19:54:54 -0600

    Convert next.config.js to simplfied form, removing styled-component dependency.

--- plan-weave/next.config.js
+++ plan-weave/next.config.js
@@ -1,31 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // TODO: Figure out how to get the dnd to work with this true
-  images: {
-    // cache optimized images for 60 seconds    
-    minimumCacheTTL: 60,  // https://www.codemotion.com/magazine/frontend/optimize-next-js-for-production/
-  },
-  compiler: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  },
+  images: { minimumCacheTTL: 60, }, // https://www.codemotion.com/magazine/frontend/optimize-next-js-for-production/
   async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin", // "same-origin-allow-popups"
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          }
-        ],
-      },
-    ]
+    return [{ source: "/(.*)", headers: [ { key: "Cross-Origin-Opener-Policy", value: "same-origin", }, { key: "Cross-Origin-Embedder-Policy", value: "require-corp",},],},]
   }, // This eliminates the Google Sign-in / Sign-up error where it says the Cross Origin is disallowed
 }
-
 module.exports = nextConfig
