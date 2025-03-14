Nellak2017/plan-weave

commit 0e904246b9cb97ba6e37350af7c5374f0d646622

Fix most linter warnings, do minor TODO items.

plan-weave/src/Application/hooks/TaskRow/useTaskRow.js
@@ -16,10 +16,10 @@ export const useTaskRow = taskID => {
     const usedTask = taskSelector(taskID)
     const { status } = usedTask || {}
     const { startTaskEditor: start, endTaskEditor: end } = timeRange()
-    const timeRangeStartEnd = { start, end } 
+    const timeRangeStartEnd = useMemo(() => ({ start, end }), [start, end])
 
     const isHighlighting = isHighlightingSelector(), isChecked = isCheckedSelector(taskID)
-    const highlight = useMemo(() => highlightTaskRow(isHighlighting, isChecked, isTaskOld(timeRangeStartEnd, usedTask)), [status, timeRangeStartEnd])
+    const highlight = useMemo(() => highlightTaskRow(isHighlighting, isChecked, isTaskOld(timeRangeStartEnd, usedTask)), [timeRangeStartEnd, isChecked, isHighlighting, usedTask])
     return { status, highlight }
 }
 // Almost done, needs: testing, batching, and completeness, as well as api stuff
@@ -31,7 +31,7 @@ export const useCompleteIcon = (taskID, currentTime) => {
         isAtleastOneTaskSelectedForDeletion
             ? handleMinOne(setMultiDeleteFSMState, fsmState)
             : handleMaxZero(setMultiDeleteFSMState, fsmState)
-    }, [isAtleastOneTaskSelectedForDeletion, setMultiDeleteFSMState, fsmState]) // NOTE: This useEffect is used to get us in or out of the choose or chosen state. It wasn't possible in the click function to my knowledge
+    }, [isAtleastOneTaskSelectedForDeletion, fsmState]) // NOTE: This useEffect is used to get us in or out of the choose or chosen state. It wasn't possible in the click function to my knowledge
     return {
         isChecked,
         handleCheckBoxClicked: () => {
@@ -53,10 +53,9 @@ export const useTaskInputContainer = taskID => {
 }
 // Almost done, needs: testing
 export const useWaste = (taskID, currentTime) => { // We calculate this from Redux state and memoize on time
-    const currentTaskRow = taskSelector?.(taskID) || {}
+    const currentTaskRow = useMemo(() => taskSelector?.(taskID) || {}, [taskID])
     const pipelineOptions = taskOrderPipeOptions()
-    const waste = useMemo(() => calculateWaste(currentTaskRow, pipelineOptions, currentTime), [pipelineOptions, currentTime])
-
+    const waste = useMemo(() => calculateWaste(currentTaskRow, pipelineOptions, currentTime), [pipelineOptions, currentTime, currentTaskRow])
     return { waste }
 }
 // Almost done, needs: testing
@@ -72,14 +71,14 @@ export const useTtc = taskID => {
 }
 // Almost done, needs: testing
 export const useEta = (taskID, currentTime) => { // We calculate this from Redux state and memoize on time
-    const currentTaskRow = taskSelector?.(taskID) || {}
+    const currentTaskRow = useMemo(() => taskSelector?.(taskID) || {}, [taskID])
     const pipelineOptions = taskOrderPipeOptions()
     const eta = useMemo(() => calculateEta(currentTaskRow, pipelineOptions, currentTime), [currentTaskRow, pipelineOptions, currentTime])
     return { eta }
 }
 // Almost done, needs: testing, correction of efficiency function
 export const useEfficiency = (taskID, currentTime) => { // We calculate this from Redux state and memoize on time
-    const currentTaskRow = taskSelector?.(taskID) || {}
+    const currentTaskRow = useMemo(() => taskSelector?.(taskID) || {}, [taskID]) 
     const pipelineOptions = taskOrderPipeOptions()
     const efficiency = useMemo(() => Math.abs(calculateEfficiency(currentTaskRow, pipelineOptions, currentTime)), [currentTaskRow, pipelineOptions, currentTime])
     return { efficiency }
@@ -108,7 +107,7 @@ export const useWeight = taskID => {
 export const useThread = taskID => {
     const { parentThread } = taskSelector?.(taskID) || {}
     const options = [] // TODO: Figure out how to do this one, I am not familiar with the registery of all threads
-    const childState = { options, defaultValue: parentThread, /* TODO: Figure out if this is the intended default, I don't recall */}
+    const childState = { options, defaultValue: parentThread, /* TODO: Figure out if this is the intended default, I don't recall */ }
     const childServices = {
         onChangeEvent: () => console.warn('onChange thunk not implemented for useThread'),
         onBlurEvent: () => console.warn('onBlur thunk not implemented for useThread'),

plan-weave/src/Application/entities/tasks/tasks.js
@@ -15,7 +15,13 @@ const tasks = createSlice({
             const taskIndex = state?.findIndex(task => task?.id === taskID)
             if (taskIndex !== -1) state[taskIndex][field] = value // Edit a task by ID and field
         },
-        // TODO: Create updateTask batch version
+        updateTasksBatch: (state, action) => {
+            const updates = action?.payload || {}
+            updates.forEach(({ taskID, field, value }) => {
+                const taskIndex = state.findIndex(task => task?.id === taskID)
+                if (taskIndex !== -1) state[taskIndex][field] = value
+            })
+        }, // Update tasks in batch
         refreshTasks: state => state?.map(task => ({ ...task, timestamp: parseISO(dateToToday(new Date(task.timestamp).toISOString())).getTime() / 1000 })), // update every task in the task list to have timestamp for today but with it's hours
         toggleSelectTask: (state, action) => {
             const { taskID } = action?.payload || {}
@@ -25,5 +31,5 @@ const tasks = createSlice({
         clearSelectedTasks: state => state?.map(task => ({ ...task, selected: false })),
     }
 })
-export const { updateTasks, addTask, deleteTask, deleteTasks, refreshTasks, updateTask, toggleSelectTask, clearSelectedTasks, } = tasks.actions
+export const { updateTasks, addTask, deleteTask, deleteTasks, refreshTasks, updateTask, toggleSelectTask, clearSelectedTasks, updateTasksBatch } = tasks.actions
 export default tasks.reducer

plan-weave/src/Application/entities/tasks/tasksThunks.js
@@ -1,10 +1,10 @@
-import { addTask, updateTask, deleteTasks } from './tasks.js'
+import { addTask, updateTask, deleteTasks, updateTasksBatch } from './tasks.js'
 import { addDnD, deleteMultipleDnD } from '../../sessionContexts/dnd.js'
-import { DEFAULT_FULL_TASK } from '../../../Core/utils/constants.js'
+import { DEFAULT_FULL_TASK, FULL_TASK_FIELDS } from '../../../Core/utils/constants.js'
 import { toggleTaskStatus, calculateWaste, calculateLiveTime, calculateEta } from '../../../Core/utils/helpers.js'
 import { toast } from 'react-toastify'
 
-// TODO: Stop hard coding the fields
+const {  completedTimeStamp, waste, liveTime, eta, task, ttc, dueDate, weight } = FULL_TASK_FIELDS
 // TODO: API + Other
 export const addTaskThunkAPI = ({ userID }) => dispatch => {
     // addTaskAPI({task, userID}) // 1. POST to API
@@ -15,33 +15,33 @@ export const addTaskThunkAPI = ({ userID }) => dispatch => {
 export const completeTaskThunkAPI = ({ userID, currentTaskRow, taskOrderPipeOptions, currentTime }) => dispatch => {
     const { id, status } = currentTaskRow || {}
     // -- calculations on incoming task for batched update below
-
     // updateTaskAPI({updatedTask, userID}) // 1. POST to API
-    dispatch(updateTask({ taskID: id, field: 'status', value: toggleTaskStatus(status) })) // 2. update the status value of the task by toggling between incomplete/complete
-    dispatch(updateTask({ taskID: id, field: 'completedTimeStamp', value: new Date().getTime() / 1000 })) // 2.1 update the completedTimeStamp value of the task
-    dispatch(updateTask({ taskID: id, field: 'waste', value: calculateWaste(currentTaskRow, taskOrderPipeOptions, currentTime) })) // 2.2 update the waste value of the task (calculated)
-    dispatch(updateTask({ taskID: id, field: 'liveTime', value: calculateLiveTime(currentTaskRow, taskOrderPipeOptions, currentTime) }))
-    dispatch(updateTask({ taskID: id, field: 'eta', value: calculateEta(currentTaskRow, taskOrderPipeOptions, currentTime) })) // update the ETA value of the task (calculated)
-    // update the Efficiency value of the task (calculated)
-    // ... basically all the read only values get updated on complete to be inline with the calculations
-    // TODO: Batch these value updates instead of individually updating each
+    dispatch(updateTasksBatch([
+        { taskID: id, field: FULL_TASK_FIELDS.status, value: toggleTaskStatus(status) },
+        { taskID: id, field: completedTimeStamp, value: new Date().getTime() / 1000 },
+        { taskID: id, field: waste, value: calculateWaste(currentTaskRow, taskOrderPipeOptions, currentTime) },
+        { taskID: id, field: liveTime, value: calculateLiveTime(currentTaskRow, taskOrderPipeOptions, currentTime) },
+        { taskID: id, field: eta, value: calculateEta(currentTaskRow, taskOrderPipeOptions, currentTime) },
+        // update the Efficiency value of the task (calculated)
+        // ... basically all the read only values get updated on complete to be inline with the calculations
+    ]))
     // dispatch(completeTaskDnD(...)) // 4. Update the dnd config 
 } // Reducer + Business Logic + Side-effects
 export const editTaskNameThunkAPI = ({ userID, taskID, taskName }) => dispatch => {
     // updateTaskAPI({updatedTask, userID}) // 1. POST to API
-    dispatch(updateTask({ taskID, field: 'task', value: taskName })) // 2. Update task by editing the name
+    dispatch(updateTask({ taskID, field: task, value: taskName })) // 2. Update task by editing the name
 }
 export const editTtcThunkAPI = ({ userID, taskID, ttc }) => dispatch => {
     // 1. POST to API
-    dispatch(updateTask({ taskID, field: 'ttc', value: ttc }))
+    dispatch(updateTask({ taskID, field: FULL_TASK_FIELDS.ttc, value: ttc }))
 }
 export const editDueThunkAPI = ({ userID, taskID, dueDate }) => dispatch => {
     // 1. POST to API
-    dispatch(updateTask({ taskID, field: 'dueDate', value: dueDate }))
+    dispatch(updateTask({ taskID, field: FULL_TASK_FIELDS.dueDate, value: dueDate }))
 }
 export const editWeightThunkAPI = ({ userID, taskID, weight }) => dispatch => {
     // 1. POST to API
-    dispatch(updateTask({ taskID, field: 'weight', value: weight }))
+    dispatch(updateTask({ taskID, field: FULL_TASK_FIELDS.weight, value: weight }))
 }
 export const deleteTasksThunkAPI = ({ userID, taskIDs }) => dispatch => {
     // 1. DELETE to API using list of taskIDs with form [{ index, id }]

plan-weave/src/Application/finiteStateMachines/MultipleDeleteButton.fsm.js
@@ -8,7 +8,7 @@ import store from '../store.js'
 const dispatch = store ? store?.dispatch : () => { }
 const defaultAction = () => dispatch(clearSelectedTasks())
 const deleteMultipleAction = (userID, taskIDsToDelete) => dispatch(deleteTasksThunkAPI({ userID, taskIDs: taskIDsToDelete }))
-export const yesAction = (setter, newState, userID, taskIDsToDelete) => () => { handleYes(setter, newState); deleteMultipleAction(userID, taskIDsToDelete); defaultAction(); }
+export const yesAction = (setter, newState, { userID, taskIDsToDelete }) => () => { handleYes(setter, newState); deleteMultipleAction(userID, taskIDsToDelete); defaultAction(); }
 export const noAction = (setter, newState) => () => { handleNo(setter, newState) }
 const modalAction = (optionHandlers = [() => console.warn('Event for yes not implemented'), () => console.warn('Event for no not implemented')]) => {
     toast.dismiss()

plan-weave/src/Application/hooks/TaskControl/useTaskControl.js
@@ -9,11 +9,11 @@ import store from '../../store.js'
 // TODO: Implement time range checking feature that makes TimePicker Redux controlled (uses checkTimeRange function)
 const dispatch = store.dispatch
 export const useTopSlot = () => {
-    const { startTaskEditor, endTaskEditor } = timeRange(), timeRangeState = { startTaskEditor, endTaskEditor }
+    const { startTaskEditor, endTaskEditor } = timeRange()
     const childState = {
         isOwl: isOwl(),
-        startTime: useMemo(() => startTaskEditor?.defaultTime ? parseISO(startTaskEditor?.defaultTime) : new Date(), [timeRangeState]),
-        endTime: useMemo(() => endTaskEditor?.defaultTime ? parseISO(endTaskEditor?.defaultTime) : new Date(), [timeRangeState]),
+        startTime: useMemo(() => startTaskEditor?.defaultTime ? parseISO(startTaskEditor?.defaultTime) : new Date(), [startTaskEditor?.defaultTime]),
+        endTime: useMemo(() => endTaskEditor?.defaultTime ? parseISO(endTaskEditor?.defaultTime) : new Date(), [endTaskEditor?.defaultTime]),
     }
     const childServices = {
         search: ({ value, id = VALID_SEARCH_IDS?.SEARCH_TASK_EDITOR_ID }) => { dispatch(searchThunk({ id, value })) },
@@ -31,11 +31,11 @@ export const useTopSlot = () => {
 }
 
 export const useBottomSlot = () => {
-    const { startTaskEditor, endTaskEditor } = timeRange(), timeRangeState = { startTaskEditor, endTaskEditor }
+    const { startTaskEditor, endTaskEditor } = timeRange()
     const userID = userIDSelector()
     const childState = {
         isFullTask: isFullTask(), fsmControlledState: fsmControlledState(), isOwl: isOwl(),
-        endTime: useMemo(() => endTaskEditor?.defaultTime ? parseISO(endTaskEditor?.defaultTime) : new Date(), [timeRangeState]),
+        endTime: useMemo(() => endTaskEditor?.defaultTime ? parseISO(endTaskEditor?.defaultTime) : new Date(), [endTaskEditor?.defaultTime]),
     }
     const childServices = {
         addTask: () => { dispatch(addTaskThunkAPI({ userID })) },

plan-weave/src/Application/selectors.js
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux'
 import { tryCatchSyncFlat, isStatusChecked } from '../Core/utils/helpers.js' // for returning defaults if no redux used
 import { CHOOSE, CHOSEN } from './finiteStateMachines/MultipleDeleteButton.fsm.js'
 // TODO: Add more business constraints here for these selectors
-// TODO: Use constants and getters for the defaults instead of hard coded values
 export const tasks = (selector = useSelector) => tryCatchSyncFlat(() => selector(state => state?.tasks), () => [])
 export const task = (taskID, selector = useSelector) => tryCatchSyncFlat(() => selector(state => state?.tasks).filter(task => task?.id === taskID)?.[0], () => { })
 export const userID = (selector = useSelector) => tryCatchSyncFlat(() => selector(state => state?.auth?.userID), () => '')

plan-weave/src/Core/utils/constants.js
@@ -44,6 +44,7 @@ export const DEFAULT_FULL_TASK = {
 	dependencies: [],
 	weight: 1,
 }
+export const FULL_TASK_FIELDS = Object.fromEntries(Object.keys(DEFAULT_FULL_TASK).map(item => [item, item])) // { id, status, ...etc}
 export const DEFAULT_TASK_CONTROL_TOOL_TIPS = {
 	owlToolTip: 'Toggle Overnight Mode', addToolTip: 'Add a New Task',
 	deleteToolTip: 'Delete selected', dropDownToolTip: 'Select Sorting Method',

plan-weave/src/UI/molecules/MultipleDeleteButton/MultipleDeleteButton.js
@@ -22,7 +22,7 @@ export const MultipleDeleteButton = ({
     const userID = userIDSelector(), taskIDsToDelete = taskIDsToDeleteSelector()
     // Handler to update state locally if uncontrolled or non-locally if controlled. It will also call the additional action we define too
     const handleStateUpdate = newState => {
-        getFSMValue(newState, ACTION)?.([yesAction(setControlledFSMState, newState, userID, taskIDsToDelete), noAction(setControlledFSMState, newState)])
+        getFSMValue(newState, ACTION)?.([yesAction(setControlledFSMState, newState, { userID, taskIDsToDelete }), noAction(setControlledFSMState, newState)])
         if (!fsmControlledState) { setFSMState(newState) } else { setControlledFSMState(newState) }
     }
     return (<>

commit bbd8365dce0e092ca7eeec2cb56653a7397dd1f6

Fix bug introduced by commit 0e90424. Fix all Linter warnings and errors.

plan-weave/src/Application/hooks/TaskRow/useTaskRow.js
@@ -53,7 +53,8 @@ export const useTaskInputContainer = taskID => {
 }
 // Almost done, needs: testing
 export const useWaste = (taskID, currentTime) => { // We calculate this from Redux state and memoize on time
-    const currentTaskRow = useMemo(() => taskSelector?.(taskID) || {}, [taskID])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const currentTaskRow = taskSelector?.(taskID) || {}
     const pipelineOptions = taskOrderPipeOptions()
     const waste = useMemo(() => calculateWaste(currentTaskRow, pipelineOptions, currentTime), [pipelineOptions, currentTime, currentTaskRow])
     return { waste }
@@ -71,14 +72,16 @@ export const useTtc = taskID => {
 }
 // Almost done, needs: testing
 export const useEta = (taskID, currentTime) => { // We calculate this from Redux state and memoize on time
-    const currentTaskRow = useMemo(() => taskSelector?.(taskID) || {}, [taskID])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const currentTaskRow = taskSelector?.(taskID) || {}
     const pipelineOptions = taskOrderPipeOptions()
     const eta = useMemo(() => calculateEta(currentTaskRow, pipelineOptions, currentTime), [currentTaskRow, pipelineOptions, currentTime])
     return { eta }
 }
 // Almost done, needs: testing, correction of efficiency function
 export const useEfficiency = (taskID, currentTime) => { // We calculate this from Redux state and memoize on time
-    const currentTaskRow = useMemo(() => taskSelector?.(taskID) || {}, [taskID]) 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const currentTaskRow = taskSelector?.(taskID) || {} 
     const pipelineOptions = taskOrderPipeOptions()
     const efficiency = useMemo(() => Math.abs(calculateEfficiency(currentTaskRow, pipelineOptions, currentTime)), [currentTaskRow, pipelineOptions, currentTime])
     return { efficiency }

plan-weave/src/Application/hooks/TaskTable/useTaskTable.js
@@ -5,9 +5,9 @@ import { isFullTask as isFullTaskSelector, taskOrderPipeOptions } from '../../se
 import { updateDnD } from '../../sessionContexts/dnd.js'
 
 const dispatch = store ? store.dispatch : () => { }
-export const useTaskTableDefault = currentTime => {
+export const useTaskTableDefault = () => {
     const pipelineOptions = taskOrderPipeOptions(), isFullTask = isFullTaskSelector()
-    const taskList = useMemo(() => taskListPipe(pipelineOptions), [currentTime, pipelineOptions])
+    const taskList = useMemo(() => taskListPipe(pipelineOptions), [pipelineOptions]) // strangely doesn't need to be memoized on currentTime. The component that calls this automatically memoizes on time due to implicit re-renders when the time changes for the parent component
     const labels = useMemo(() => getHeaderLabels(isFullTask), [isFullTask])
     const renderNumber = useMemo(() => getTaskRenderNumber(isFullTask), [isFullTask])
     return {

plan-weave/src/UI/atoms/TimePickerWrapper/TimePickerWrapper.js
@@ -14,8 +14,10 @@ export const TimePickerWrapper = ({
 }) => {
   const initialTime = useRef(parse(defaultTime, 'HH:mm', new Date()))
   const [time, setTime] = useState(initialTime.current)
+  /* eslint-disable react-hooks/exhaustive-deps */
   const debouncedTime = useMemo(() => time, [time / 10]) // divide by 10 to be able to debounce on first decimal place of change
   useEffect(() => { onTimeChange?.(debouncedTime) }, [debouncedTime]) // only call parent when clock hand changes
+  /* eslint-disable react-hooks/exhaustive-deps */
   const [fsmState, setFsmState] = useState(getDefaultState()) // the current state of the state machine
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>

plan-weave/src/UI/molecules/TaskTable/TaskTable.js
@@ -36,7 +36,7 @@ export const TaskTable = ({
 }
 
 export const TaskTableDefault = ({ currentTime, customHook = useTaskTableDefault }) => {
-    const { childState, childServices } = customHook?.(currentTime) || {}
+    const { childState, childServices } = customHook?.() || {}
     const { taskList, labels, renderNumber } = childState || {}
     const { onDragEndEvent } = childServices || {}
     return (