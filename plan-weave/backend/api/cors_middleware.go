89ee114c045054bf50d00fcd931f10f5b96c7f5a
Connor Keenum <nellakprime@gmail.com>
2025-07-01 20:09:39 -0500

    Fix CORS middleware.

--- plan-weave/backend/api/cors_middleware.go
+++ plan-weave/backend/api/cors_middleware.go
@@ -7,6 +7,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Allow requests from your frontend origin(s)
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Change to your frontend origin in prod
+		w.Header().Set("Access-Control-Allow-Origin", "https://planweave.com") // Change to your frontend origin in prod
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true") // if credentials are used (cookies/auth headers)
