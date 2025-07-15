8a7a8d32f7b7182ec9153e9c18e7fc56226ee950
Connor Keenum <nellakprime@gmail.com>
2025-07-15 15:54:48 -0500

    chore: Remove start time in Redux initial state for timePickerContext for the 'remove Start Clock' refactor.

--- plan-weave/src/Application/boundedContexts/timeRange/timeRangeSlice.js
+++ plan-weave/src/Application/boundedContexts/timeRange/timeRangeSlice.js
@@ -3,10 +3,10 @@ import { parse } from "date-fns"
 import { VALID_TIMERANGE_IDS } from "../../validIDs"
 import { dateToToday, endPlusOne } from "../../../Core/utils/helpers"
 
+// TODO: Add feature that lets user update end time and if it is overnight or not and persist it with DB
 const timePickerContext = createSlice({
     name: 'timePickerContext',
     initialState: {
-        [VALID_TIMERANGE_IDS.START_TIME_PICKER_ID]: { defaultTime: parse('10:00', 'HH:mm', new Date()).toISOString() },
         [VALID_TIMERANGE_IDS.END_TIME_PICKER_ID]: { defaultTime: parse('23:00', 'HH:mm', new Date()).toISOString() },
     }, // { [id]: { defaultTime: 'HH:MM' }}
     reducers: {
@@ -14,11 +14,10 @@ const timePickerContext = createSlice({
             const { id, value } = action.payload
             if (Object.values(VALID_TIMERANGE_IDS).includes(id)) { state[id].defaultTime = value }
         },
-        refreshTimePickers: (state, action) => { // Update start to be for today and end to be for today if no owl, and end is tomorrow if owl
+        refreshTimePickers: (state, action) => { // Update end to be for today if no owl, and end is tomorrow if owl
             const { isOwl } = action.payload
-            const { START_TIME_PICKER_ID, END_TIME_PICKER_ID } = VALID_TIMERANGE_IDS
-            const oldStart = state[START_TIME_PICKER_ID].defaultTime, oldEnd = state[END_TIME_PICKER_ID].defaultTime
-            state[START_TIME_PICKER_ID].defaultTime = dateToToday(oldStart)
+            const { END_TIME_PICKER_ID } = VALID_TIMERANGE_IDS
+            const oldEnd = state[END_TIME_PICKER_ID].defaultTime
             state[END_TIME_PICKER_ID].defaultTime = isOwl ? endPlusOne(oldEnd) : dateToToday(oldEnd)
         }
     }
