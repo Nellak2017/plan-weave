7253fcf109207d94c6492f6f0fef78382728119c
Connor Keenum <nellakprime@gmail.com>
2025-07-17 15:10:13 -0500

    Make Password requirements tests Green.

--- plan-weave/cypress/e2e/password-requirements.cy.js
+++ plan-weave/cypress/e2e/password-requirements.cy.js
@@ -1,20 +1,23 @@
 describe('Password Requirements Validation', () => {
     const email = 'fake@example.com' // We don’t care if email is valid here
     const passwordCases = [
-        { value: 'Ab1!', message: 'Password must be at least 8 characters' },
-        { value: 'A1!'.repeat(50), message: 'Password must be no more than 100 characters' },
-        { value: 'password1!', message: 'Password must include an uppercase letter' },
-        { value: 'PASSWORD1!', message: 'Password must include a lowercase letter' },
-        { value: 'Password!', message: 'Password must include a number' },
+        { value: 'Ab1!', message: 'Password should be at least 8 characters' },
+        { value: 'A1!'.repeat(50), message: 'Password cannot be longer than 72 characters' },
+        { value: 'password1!', message: 'at least one character of each:' }, // lower case
+        { value: 'PASSWORD1!', message: 'at least one character of each:' }, // capital letters
+        { value: 'Password!', message: 'at least one character of each:' }, // digits
     ]
-    const paths = ['/login', '/signup']
+    // const testedMessage = ''
+    const paths = ['/signup'] // Login excluded because it should just say invalid credentials!
     paths.forEach(path => {
         describe(`Form: ${path}`, () => {
             beforeEach(() => {
                 cy.visit(path)
                 cy.get('#email').type(email)
             })
-            passwordCases.forEach(({ value, message }) => {
+            passwordCases
+            // .filter(({ message }) => message === testedMessage) // so this is only run
+            .forEach(({ value, message }) => {
                 it(`should show error: ${message}`, () => {
                     cy.get('#password').clear().type(value)
                     cy.get('#email-auth').click()
